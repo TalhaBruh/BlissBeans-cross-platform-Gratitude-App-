d7a4a4e27763f4525931708a71ac39b09d29da24
Talha Hussain <talhahussain349@gmail.com>
2024-05-29 23:51:06 +0500

    Add Donation screen to exports and reorganize component exports

---
+++ src/screens/index.js
@@ -0,0 +1,21 @@
+import Welcome from "./Welcome";
+import OnboardingScreen from "./OnboardingScreen";
+import SignUpScreen from "./Auth/SignUp";
+import SignInScreen from "./Auth/SignIn";
+import RegisterScreen from "./Auth/Register";
+import ForgotPassScreen from "./Auth/Forgot";
+import Basic  from "./AddMedicine/Basic";
+import LogoutScreen from "./Settings";
+import Donation from "./Donation";
+
+export {
+  Welcome,
+  OnboardingScreen,
+  SignUpScreen,
+  SignInScreen,
+  RegisterScreen,
+  ForgotPassScreen,
+  Basic,
+  LogoutScreen,
+  Donation
+};
