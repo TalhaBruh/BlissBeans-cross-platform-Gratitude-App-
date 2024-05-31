ff16b8240d2d8e3d78ce1218f4c15917d09dea78
Talha Hussain <talhahussain349@gmail.com>
2024-06-01 00:49:42 +0500

    Optimized Forgot password

--- src/screens/Auth/Forgot.jsx
+++ src/screens/Auth/Forgot.jsx
@@ -1,25 +1,37 @@
-import { SafeAreaView, Text, View, TouchableOpacity,Image, TextInput } from "react-native"
-import {ForgotPass} from '../../assets/authScreens'
-import { useNavigation } from "@react-navigation/native"
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import { ForgotPass } from "../../assets/authScreens";
+import { useNavigation } from "@react-navigation/native";
 const ForgotPassScreen = () => {
-    const navigation = useNavigation()
-    const handleForgotPass = async ()=>{
-     
-     navigation.navigate('SignIn')
-    }
-    return (
-    <SafeAreaView className= 'flex-1 items-center justify-center bg-[#eae8e5]'>
-        <View>
-            <Image source={ForgotPass} className ='h-[150px] w-[150px] mb-10'/>
-        </View>
-        <View>
-            <TextInput placeholder="email" className='bg-gray-100  text-center py-2 rounded-full m-2 ' />            
-            <TouchableOpacity onPress={handleForgotPass} className='bg-red-600 px-[30%] py-2 rounded-full m-2'>
-                <Text className = 'text-white text-lg uppercase'>Recover</Text>
-            </TouchableOpacity>
-        </View>
+  const navigation = useNavigation();
+  const handleForgotPass = async () => {
+    navigation.navigate("SignIn");
+  };
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-[#eae8e5]">
+      <View>
+        <Image source={ForgotPass} className="h-[150px] w-[150px] mb-10" />
+      </View>
+      <View>
+        <TextInput
+          placeholder="email"
+          className="bg-gray-100  text-center py-2 rounded-full m-2 "
+        />
+        <TouchableOpacity
+          onPress={handleForgotPass}
+          className="bg-red-600 px-[30%] py-2 rounded-full m-2"
+        >
+          <Text className="text-white text-lg uppercase">Recover</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default ForgotPassScreen
+export default ForgotPassScreen;
