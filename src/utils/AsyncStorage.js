49bad350a809b9e158bbdfcde1a1eb26258a34ff
Talha Hussain <talhahussain349@gmail.com>
2024-05-31 05:28:18 +0500

    formatting with prettier

--- src/utils/AsyncStorage.js
+++ src/utils/AsyncStorage.js
@@ -1,31 +1,27 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+export const setItem = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (error) {
+    console.log("Error");
+  }
+};
 
-export const setItem = async (key,value)=>{
-    try {
-        await AsyncStorage.setItem(key,value)
-    } catch (error) {
-        console.log("Error")
-    }
-}
+export const getItem = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (error) {
+    console.log("Error");
+  }
+};
 
-
-export const getItem = async (key)=>{
-    try {
-      const value =   await AsyncStorage.getItem(key)
-      return value
-    } catch (error) {
-        console.log("Error")
-    }
-}
-
-
-
-export const removeItem = async (key)=>{
-    try {
-      const value =   await AsyncStorage.removeItem(key)
-      return value
-    } catch (error) {
-        console.log("Error")
-    }
-}
+export const removeItem = async (key) => {
+  try {
+    const value = await AsyncStorage.removeItem(key);
+    return value;
+  } catch (error) {
+    console.log("Error");
+  }
+};
