07a86af302e168d532819f5813bf9d0feca6faed
Talha Hussain <talhahussain349@gmail.com>
2024-05-31 05:32:23 +0500

    module added in donation.js

--- src/screens/Donation.js
+++ src/screens/Donation.js
@@ -13,29 +13,36 @@ import Document from "../assets/setup-icon.png";
 import beansPlate from "../assets/beansPlate.png";
 import donationHeader from "../assets/DonationHeader.png";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
 import { setup } from "../assets/Sounds/AllSounds";
-import { Apple, English, Facebook, German, Instagram, Paypal, Tiktok } from "../assets/SocialMedaiLinks/linkimages";
-import * as Font from 'expo-font';
+import {
+  Apple,
+  English,
+  Facebook,
+  German,
+  Instagram,
+  Paypal,
+  Tiktok,
+} from "../assets/SocialMedaiLinks/linkimages";
+import * as Font from "expo-font";
 import { LanguageContext, useLanguage } from "../context/LanguageContext";
 import { setItem } from "../utils/AsyncStorage";
-import * as WebBrowser from 'expo-web-browser';
-import * as Linking from 'expo-linking';
-
+import * as WebBrowser from "expo-web-browser";
+import * as Linking from "expo-linking";
 
 const Donation = () => {
   const navigation = useNavigation();
   const [sound, setSound] = useState();
-  async function playSound (audioWillBe) {
-    console.log('Loading Sound');
+  async function playSound(audioWillBe) {
+    console.log("Loading Sound");
     const { sound } = await Audio.Sound.createAsync(audioWillBe);
     setSound(sound);
-    console.log('Playing Sound');
+    console.log("Playing Sound");
     await sound.playAsync();
   }
 
   const handleCenterPress = () => {
-    playSound(setup)
+    playSound(setup);
     navigation.navigate("Home");
   };
 
@@ -54,21 +61,20 @@ const Donation = () => {
   }, []);
 
   const [fontsLoaded, fontError] = Font.useFonts({
-    'PaytoneOne': require('../../assets/fonts/PaytoneOne-Regular.ttf'),
+    PaytoneOne: require("../../assets/fonts/PaytoneOne-Regular.ttf"),
   });
 
-  if(!fontsLoaded){
+  if (!fontsLoaded) {
     // alert("Aaaaaaaaaa = ",fontsLoaded )
     // return null
   }
 
-
   const { language, setLanguage } = useContext(LanguageContext);
   const handleLanguageChange = (newLanguage) => {
     setLanguage(newLanguage);
-    if(newLanguage == 'english'){
+    if (newLanguage == "english") {
       setItem("language", "1");
-    }else{
+    } else {
       setItem("language", "0");
     }
   };
@@ -108,19 +114,30 @@ const Donation = () => {
               ? `Make a donation`
               : "Spende an den Entwickler"}
           </Text>
-          
-            {language == "english" ? 
-            <Text style={styles.donationFont}>Your donations are what allows to achieve our goals at the BlissBeans project. The best way to support me is making a donation or by signing up to Patreon and becoming a monthly donator. By doing this you'll also recieve a unique monthly gift to you!</Text> : (
-              <>
-                <View style={{width:"100%"}}>
-                  <Text style={styles.donationFont}>{`Mit Deiner Spende kann ich BlissBeans weiterentwicklen. Der beste Weg, mich zu unterstützen, ist eine Spende :-)`}</Text>
-                </View>
-                <View style={{width:"100%"}}>
-                  <Text style={styles.donationFont}>{"Folge Mir auch auf Instagram, Facebook und TikTok!"}</Text>
-                </View>
-              </>
-            )}
-          
+
+          {language == "english" ? (
+            <Text style={styles.donationFont}>
+              Your donations are what allows to achieve our goals at the
+              BlissBeans project. The best way to support me is making a
+              donation or by signing up to Patreon and becoming a monthly
+              donator. By doing this you'll also recieve a unique monthly gift
+              to you!
+            </Text>
+          ) : (
+            <>
+              <View style={{ width: "100%" }}>
+                <Text
+                  style={styles.donationFont}
+                >{`Mit Deiner Spende kann ich BlissBeans weiterentwicklen. Der beste Weg, mich zu unterstützen, ist eine Spende :-)`}</Text>
+              </View>
+              <View style={{ width: "100%" }}>
+                <Text style={styles.donationFont}>
+                  {"Folge Mir auch auf Instagram, Facebook und TikTok!"}
+                </Text>
+              </View>
+            </>
+          )}
+
           <View
             style={{
               flexDirection: "row",
@@ -210,9 +227,7 @@ const Donation = () => {
                   // WebBrowser.openBrowserAsync(
                   //   "https://www.tiktok.com/@blissbeans.me"
                   // )
-                  Linking.openURL(
-                    "https://www.tiktok.com/@blissbeans.me"
-                  )
+                  Linking.openURL("https://www.tiktok.com/@blissbeans.me")
                 }
               >
                 <Image
@@ -227,17 +242,24 @@ const Donation = () => {
       </View>
 
       <View style={styles.bottomSection}>
-        <TouchableOpacity onPress={()=>{
-          navigation.navigate('Donation');
-        }}>
-          <Image source={Document} style={[styles.icon,{width:50,height:50}]} />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Donation");
+          }}
+        >
+          <Image
+            source={Document}
+            style={[styles.icon, { width: 50, height: 50 }]}
+          />
         </TouchableOpacity>
         <TouchableOpacity onPress={handleCenterPress}>
           <Image source={beansPlate} style={styles.largeIcon} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{
-          navigation.navigate('Diary');
-        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Diary");
+          }}
+        >
           <Image
             source={diary}
             style={[styles.icon, { width: 45, height: 40 }]}
@@ -251,7 +273,7 @@ const Donation = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#cecac1'
+    backgroundColor: "#cecac1",
     // backgroundColor:'red'
   },
   topSection: {
@@ -283,9 +305,9 @@ const styles = StyleSheet.create({
   },
   topHeader: {
     width: "100%",
-    height: '20%',
+    height: "20%",
     // backgroundColor:'yellow',
-    paddingHorizontal:20
+    paddingHorizontal: 20,
   },
   separator: {
     borderBottomColor: "#9a6041",
@@ -293,33 +315,41 @@ const styles = StyleSheet.create({
     width: "95%",
     // marginLeft: 30,
     marginTop: 10,
-    alignSelf:"center"
+    alignSelf: "center",
   },
-  languageView:{
+  languageView: {
     // backgroundColor:'skyblue',
-    height:"25%",width:'100%',
-    justifyContent:"space-around",
+    height: "25%",
+    width: "100%",
+    justifyContent: "space-around",
     // alignItems:'center'
-    padding:10
+    padding: 10,
   },
-  Links:{
+  Links: {
     // backgroundColor:'pink',
-    height:"20%",width:'100%',
-    justifyContent:"space-around",
+    height: "20%",
+    width: "100%",
+    justifyContent: "space-around",
     // alignItems:'center'
-    padding:10
+    padding: 10,
   },
-  flag:{
-    height:100,width:100,borderRadius:50
+  flag: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
   },
-  font:{fontFamily:'PaytoneOne',fontSize: 22, color:'#e87353',marginHorizontal:10},
-  donationFont :{
+  font: {
+    fontFamily: "PaytoneOne",
+    fontSize: 22,
+    color: "#e87353",
+    marginHorizontal: 10,
+  },
+  donationFont: {
     fontFamily: "PaytoneOne",
     fontSize: 10,
     color: "#9a6041",
     marginHorizontal: 10,
-  }
-  
+  },
 });
 
 export default Donation;
