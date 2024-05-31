e6d4aff372b11e86e8e9ef391b39ba5b359bcc5d
Talha Hussain <talhahussain349@gmail.com>
2024-05-31 05:27:38 +0500

    formatting with prettier

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
-import { MedicineProvider } from './src/context/ContextProvider';
-import AppNavigation from './src/navigation/AppNavigation';
-import React , {useEffect} from 'react';
-import { setItem } from './src/utils/AsyncStorage';
+import { MedicineProvider } from "./src/context/ContextProvider";
+import AppNavigation from "./src/navigation/AppNavigation";
+import React, { useEffect } from "react";
+import { setItem } from "./src/utils/AsyncStorage";
 export default function App() {
-
-  useEffect (() => {
-    setItem('onboarded', '0');
-  } , [])
-  return (
-    <AppNavigation/>
-  );
+  useEffect(() => {
+    setItem("onboarded", "0");
+  }, []);
+  return <AppNavigation />;
 }
