1d604ba4683b1a9b6ffeee896e7d0da8ccbaf2b5
Talha Hussain <talhahussain349@gmail.com>
2024-06-02 00:36:20 +0500

    code updated with prettier.

--- src/navigation/AddMedicineNavigator.js
+++ src/navigation/AddMedicineNavigator.js
@@ -1,43 +1,31 @@
 // AddMedicineNavigator.js
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import FirstQuestionScreen from './FirstQuestion';
-import SecondQuestionScreen from './SecondQuestion';
-import ThirdQuestionScreen from './ThirdQuestion';
-import FourthQuestionScreen from './FourthQuestion';
-import FifthQuestionScreen from './FifthQuestion';
-import SixthQuestionScreen from './SixthQuestion';
-import SeventhQuestionScreen from './SeventhQuestion';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import FirstQuestionScreen from "./FirstQuestion";
+import SecondQuestionScreen from "./SecondQuestion";
+import ThirdQuestionScreen from "./ThirdQuestion";
+import FourthQuestionScreen from "./FourthQuestion";
+import FifthQuestionScreen from "./FifthQuestion";
+import SixthQuestionScreen from "./SixthQuestion";
+import SeventhQuestionScreen from "./SeventhQuestion";
 // Import the other screen components here
 
 const Stack = createStackNavigator();
 
 const AddMedicineNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen
-      name="FirstQuestionScreen"
-      component={FirstQuestionScreen}
-    />
+    <Stack.Screen name="FirstQuestionScreen" component={FirstQuestionScreen} />
     <Stack.Screen
       name="SecondQuestionScreen"
       component={SecondQuestionScreen}
     />
-    <Stack.Screen
-      name="ThirdQuestionScreen"
-      component={ThirdQuestionScreen}
-    />
+    <Stack.Screen name="ThirdQuestionScreen" component={ThirdQuestionScreen} />
     <Stack.Screen
       name="FourthQuestionScreen"
       component={FourthQuestionScreen}
     />
-    <Stack.Screen
-      name="FifthQuestionScreen"
-      component={FifthQuestionScreen}
-    />
-    <Stack.Screen
-      name="SixthQuestionScreen"
-      component={SixthQuestionScreen}
-    />
+    <Stack.Screen name="FifthQuestionScreen" component={FifthQuestionScreen} />
+    <Stack.Screen name="SixthQuestionScreen" component={SixthQuestionScreen} />
     <Stack.Screen
       name="SeventhQuestionScreen"
       component={SeventhQuestionScreen}
