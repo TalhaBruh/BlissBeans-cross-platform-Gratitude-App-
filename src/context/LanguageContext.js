7664073167d24c1bf97d77ac3e623c21c29d9b6a
Talha Hussain <talhahussain349@gmail.com>
2024-06-04 16:03:25 +0500

    Optimized the language Context Controller

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,12 +1,12 @@
 // LanguageContext.js
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 // Create the context
 export const LanguageContext = createContext();
 
 // Create the provider component
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState('English');
+  const [language, setLanguage] = useState("English");
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
