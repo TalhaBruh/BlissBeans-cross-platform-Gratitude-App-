25f6ad090b603c5f1a79622764f6154494255cdd
Talha Hussain <talhahussain349@gmail.com>
2024-06-06 00:15:21 +0500

    Time Picker Adjusted

--- src/components/TimePicker.jsx
+++ src/components/TimePicker.jsx
@@ -1,20 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 
-const hoursArray = Array.from({length: 12}, (_, i) => i + 1);
-const minutesArray = Array.from({length: 60}, (_, i) => i);
-const amPmArray = ['AM', 'PM'];
+const hoursArray = Array.from({ length: 12 }, (_, i) => i + 1);
+const minutesArray = Array.from({ length: 60 }, (_, i) => i);
+const amPmArray = ["AM", "PM"];
 
 const TimePicker = ({ onTimeChange }) => {
   const date = new Date();
   const [selectedHour, setSelectedHour] = useState(date.getHours() % 12 || 12);
   const [selectedMinute, setSelectedMinute] = useState(date.getMinutes());
-  const [selectedAmPm, setSelectedAmPm] = useState(date.getHours() >= 12 ? 'PM' : 'AM');
+  const [selectedAmPm, setSelectedAmPm] = useState(
+    date.getHours() >= 12 ? "PM" : "AM"
+  );
 
   const viewConfigRef = useRef({ viewAreaCoveragePercentThreshold: 50 });
   useEffect(() => {
     const date = new Date();
-    date.setHours(selectedHour + (selectedAmPm === 'PM' ? 12 : 0));
+    date.setHours(selectedHour + (selectedAmPm === "PM" ? 12 : 0));
     date.setMinutes(selectedMinute);
     onTimeChange(date);
   }, [selectedHour, selectedMinute, selectedAmPm]);
@@ -39,30 +41,57 @@ const TimePicker = ({ onTimeChange }) => {
 
   const onScrollAmPm = useRef((event) => {
     const offsetY = event.nativeEvent.contentOffset.y;
-    setSelectedAmPm(offsetY < 15 ? 'AM' : 'PM');
+    setSelectedAmPm(offsetY < 15 ? "AM" : "PM");
   });
-
 
   const renderHour = ({ item }) => (
     <TouchableOpacity>
-      <Text style={{ fontSize: 50, color: item === selectedHour ? 'yellow' : 'gray' }}>{item}</Text>
+      <Text
+        style={{
+          fontSize: 50,
+          color: item === selectedHour ? "yellow" : "gray",
+        }}
+      >
+        {item}
+      </Text>
     </TouchableOpacity>
   );
 
   const renderMinute = ({ item }) => (
     <TouchableOpacity>
-      <Text style={{ fontSize: 50, color: item === selectedMinute ? 'yellow' : 'gray' }}>{item}</Text>
+      <Text
+        style={{
+          fontSize: 50,
+          color: item === selectedMinute ? "yellow" : "gray",
+        }}
+      >
+        {item}
+      </Text>
     </TouchableOpacity>
   );
 
   const renderAmPm = ({ item }) => (
     <TouchableOpacity>
-      <Text style={{ fontSize: 50, color: item === selectedAmPm ? 'yellow' : 'gray' }}>{item}</Text>
+      <Text
+        style={{
+          fontSize: 50,
+          color: item === selectedAmPm ? "yellow" : "gray",
+        }}
+      >
+        {item}
+      </Text>
     </TouchableOpacity>
   );
 
   return (
-    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 100 }}>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 100,
+      }}
+    >
       <FlatList
         data={hoursArray}
         renderItem={renderHour}
@@ -80,13 +109,13 @@ const TimePicker = ({ onTimeChange }) => {
         viewabilityConfig={viewConfigRef.current}
       />
       <FlatList
-  data={amPmArray}
-  renderItem={renderAmPm}
-  keyExtractor={(item) => item}
-  onScroll={onScrollAmPm.current}
-/>
+        data={amPmArray}
+        renderItem={renderAmPm}
+        keyExtractor={(item) => item}
+        onScroll={onScrollAmPm.current}
+      />
     </View>
   );
-}
+};
 
 export default TimePicker;
